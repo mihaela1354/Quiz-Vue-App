d62b63cbc45a73a6ddb37e5d416c082b412e369e
Mihaela <mihaeladinkova@abv.bg>
2019-08-30 13:49:53 +0300

    Register styles

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -1,60 +1,16 @@
 <template>
-  <div>
-    <div>
-      <div>
-        <div>
-          <h3>Registration</h3>
-          <div class="d-flex justify-content-end social_icon">
-            <span>
-              <i class="fab fa-facebook-square"></i>
-            </span>
-            <span>
-              <i class="fab fa-google-plus-square"></i>
-            </span>
-            <span>
-              <i class="fab fa-twitter-square"></i>
-            </span>
-          </div>
-        </div>
-        <div>
-          <form >
-            <div>
-              <div>
-                <span>
-                  <i class="fas fa-user"></i>
-                </span>
-              </div>
-              <input type="text" name="username" placeholder="username" />
-            </div>
-            <div>
-              <div>
-                <span>
-                  <i class="fas fa-key"></i>
-                </span>
-              </div>
-              <input type="password" name="password" placeholder="password" />
-            </div>
-            <div>
-              <div>
-                <span>
-                  <i class="fas fa-key"></i>
-                </span>
-              </div>
-              <input type="password" name="rePassword" placeholder="re-password" />
-            </div>
-
-            <input type="submit" value="Register" />
-          </form>
-        </div>
-        <div>
-          <div>
-            Already have an account?
-            <router-link class="text-warning" to="/login">Login</router-link>
-          </div>
-        </div>
-      </div>
+<div>
+    <h3>Register</h3>
+    <form @submit.prevent="register">
+        <input class="input" v-model="username" type="text" name="username" placeholder="username" />
+        <input class="input" v-model="password" type="password" name="password" placeholder="password" />
+      <input class="register" type="submit" value="Register" />
+      <div class="aditional">
+      You are already registered?
+      <router-link class="text-warning" to="/login">Login</router-link>
     </div>
-  </div>
+    </form>
+</div>
 </template>
 
 <script>
@@ -62,4 +18,47 @@ export default {};
 </script>
 
 <style scoped>
+*{
+  display:block;
+  width: 60%;
+  margin:auto;
+  text-align: center;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+h3{
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
+}
+form {
+  background-color: rgb(226, 225, 225);
+  box-shadow: 0 0 40px -10px #000;
+  margin-bottom: 12rem;
+  padding: 2rem 5rem 2rem 5rem;
+}
+.input{
+  margin:auto;
+  background-color: rgba(139, 195, 176, 0.171);
+  width: 73%;
+  margin-bottom: 12px;
+  margin-top: 12px;
+  border-radius: 5px;
+  padding: 15px;
+}
+.register{
+  background-color: rgba(193, 214, 207, 0.603);
+  border-color:green;
+  width: 80%;
+  margin-bottom: 12px;
+  margin-top: 12px;
+  border-radius: 5px;
+  padding: 15px;
+}
+.aditional a{
+  text-decoration: none;
+  color:green;
+}
+.aditional a:hover{
+  cursor: pointer;
+  font-size: 110%;
+}
 </style>
