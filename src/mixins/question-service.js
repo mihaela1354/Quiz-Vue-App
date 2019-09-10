08964086b93b5d3ebf5bec64b7fb4d1c86ab1e29
Kiril <kiril.98.kirilov@gmail.com>
2019-09-11 01:18:30 +0300

    Add Question (fixes)
    * add id to questionIds array

--- src/mixins/question-service.js
+++ src/mixins/question-service.js
@@ -16,8 +16,18 @@ export const questionService = {
 
       requester.post('questions', 'appdata', 'Kinvey', payload)
         .then((res) => res.json())
-        .then(() => {
-          this.$router.push('/');
+        .then((question) => {
+          requester.get(`quizzes/${this.$route.params.id}`, 'appdata', 'Kinve')
+            .then((res) => res.json())
+            .then((quiz) => {
+              quiz.questionIds.push(question._id);
+
+              return requester.put(`quizzes/${this.$route.params.id}`, 'appdata', 'Kinvey', quiz)
+            })
+            .then((res) => res.json())
+            .then(() => {
+              this.$router.push('/');
+            });
         })
         .catch(console.error);
     }
